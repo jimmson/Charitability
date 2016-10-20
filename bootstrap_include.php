61a7e5a1912ecceceb2203483c089cca633d8918
James Campbell <jimmycampbell1991@gmail.com>
2016-10-20 15:18:25 +0200

    Slashes
    Slashes

--- bootstrap_include.php
+++ bootstrap_include.php
@@ -1,17 +1,17 @@
 <?php
 
 
-require("bootstrap\controls\binput.php");
-require("bootstrap\controls\btextarea.php");
-require("bootstrap\controls\bspan.php");
-require("bootstrap\controls\bp.php");
-require("bootstrap\containers\bselect.php");
-require("bootstrap\containers\btab.php");
-require("bootstrap\containers\btabs.php");
-require("bootstrap\containers\bcarousel.php");
-require("bootstrap\containers\bpanel.php");
+require("bootstrap/controls/binput.php");
+require("bootstrap/controls/btextarea.php");
+require("bootstrap/controls/bspan.php");
+require("bootstrap/controls/bp.php");
+require("bootstrap/containers/bselect.php");
+require("bootstrap/containers/btab.php");
+require("bootstrap/containers/btabs.php");
+require("bootstrap/containers/bcarousel.php");
+require("bootstrap/containers/bpanel.php");
 
-require("bootstrap\layouts\grid.php");
-require("bootstrap\layouts\\tab.php");
-require("bootstrap\layouts\carousel.php");
-require("bootstrap\layouts\panel.php");
+require("bootstrap/layouts/grid.php");
+require("bootstrap/layouts/tab.php");
+require("bootstrap/layouts/carousel.php");
+require("bootstrap/layouts/panel.php");
